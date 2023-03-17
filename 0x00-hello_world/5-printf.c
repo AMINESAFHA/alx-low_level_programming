e4f320204d9a637adf5276931150b9c1199daa2f
AMINESAFHA <125523774+AMINESAFHA@users.noreply.github.com>
2023-03-17 02:46:59 +0100

    Add files via upload

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * This function is the entry point of the program.
+ * It prints a message to the standard output.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
